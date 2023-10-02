8bb5bb156ce404c9f90d553a592c0a485b96bfef
273Do <keita.y310@icloud.com>
2023-10-03 00:55:08 +0900

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                "resources/css/app.css",
+                "resources/scss/app.scss",
+                "resources/js/app.tsx",
+            ],
+            refresh: true,
+        }),
+        react(),
+    ],
+});
