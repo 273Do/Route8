f67f50193a5cfdffd0ef6913c5fe65eb987b991d
273Do <keita.y310@icloud.com>
2023-11-17 00:59:37 +0900

    first commit

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -13,15 +13,12 @@ class BookmarkController extends Controller
         if (!$user->is_bookmark($post)) {
             $user->bookmark_posts()->attach($post);
         }
-        // return Redirect::back();
-        // return back();
     }
     public function destroy($post) {
         $user = \Auth::user();
         if ($user->is_bookmark($post)) {
             $user->bookmark_posts()->detach($post);
         }
-        // return back();
     }
 }
 // ブックマークした記事の一覧を取得から->
