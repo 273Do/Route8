0a6ce484fbcacd7476773ffe7f02a4cb988326dd
273Do <keita.y310@icloud.com>
2023-11-22 12:46:18 +0900

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,9 +3,19 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-         <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        
+        <meta
+         property="og:description"
+         content="Route8は走行時の状態や移動手段なども合わせて経路を共有サイトです。走行前でも投稿できます。"
+        />
+        
+        <link rel="icon" href="{{ asset('/route8_favicon32.ico')}}" />
+        <link rel="apple-touch-icon" href="{{ asset('/route8_favicon128.ico')}}"  />
+        
+        <title>Route8</title>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <!--<title inertia>{{ config('app.name', 'Laravel') }}</title>-->
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
